abe5c74fe46ecf3d4e90d0fea3db7314106fe345
edafevzioglu <164877604+edafevzioglu@users.noreply.github.com>
2025-06-17 23:44:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.seat:not(.seat-sold)').forEach(seat => {
-    seat.addEventListener('click', () => seat.classList.toggle('seat-selected'));
-  });
+    const seats = document.querySelectorAll('.seat:not(.seat-sold)'); // Select all available seats
+
+    // Add click event to each seat
+    seats.forEach(seat => {
+        seat.addEventListener('click', () => {
+            seat.classList.toggle('seat-selected');
+        });
+    });
 });
